e2293642e2026ec89ec921b4855eca4c2e3ceaa6
Jonathan Harvey <jonathanharvey22@gmail.com>
2023-08-15 13:16:18 -0700

    initial commit for portfolio site starting files

---
+++ assets/js/main.js
@@ -0,0 +1,123 @@
+/*
+	Twenty by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$header = $('#header'),
+		$banner = $('#banner');
+
+	// Breakpoints.
+		breakpoints({
+			wide:      [ '1281px',  '1680px' ],
+			normal:    [ '981px',   '1280px' ],
+			narrow:    [ '841px',   '980px'  ],
+			narrower:  [ '737px',   '840px'  ],
+			mobile:    [ null,      '736px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Scrolly.
+		$('.scrolly').scrolly({
+			speed: 1000,
+			offset: function() { return $header.height() + 10; }
+		});
+
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			mode: 'fade',
+			noOpenerFade: true,
+			expandMode: (browser.mobile ? 'click' : 'hover')
+		});
+
+	// Nav Panel.
+
+		// Button.
+			$(
+				'<div id="navButton">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
+
+		// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+			if (browser.os == 'wp' && browser.osVersion < 10)
+				$('#navButton, #navPanel, #page-wrapper')
+					.css('transition', 'none');
+
+	// Header.
+		if (!browser.mobile
+		&&	$header.hasClass('alt')
+		&&	$banner.length > 0) {
+
+			$window.on('load', function() {
+
+				$banner.scrollex({
+					bottom:		$header.outerHeight(),
+					terminate:	function() { $header.removeClass('alt'); },
+					enter:		function() { $header.addClass('alt reveal'); },
+					leave:		function() { $header.removeClass('alt'); }
+				});
+
+			});
+
+		}
+
+})(jQuery);
+
+let emailButton = document.querySelector("#email-button")
+let contactButton = document.querySelector("#contact-button")
+let estimateButton = document.querySelector("#estimate-button")
+let overlay = document.querySelector('#overlay')
+let cancel = document.querySelector('#cancel')
+let contactButtons = document.querySelectorAll("a[href='#contact']");
+
+emailButton.addEventListener('click', toggleOverlay)
+contactButton.addEventListener('click', toggleOverlay)
+estimateButton.addEventListener('click', toggleOverlay)
+cancel.addEventListener('click', toggleOverlay)
+
+contactButtons[0].addEventListener('click', toggleOverlay)
+contactButtons[1].addEventListener('click', toggleOverlay)
+
+function toggleOverlay(click){
+	if(click.target.type === "submit"){
+		console.log("testing submit button")
+	}
+	if (overlay.style.display == "block"){
+		overlay.style.display = "none"
+	}
+	else{
+		overlay.style.display = "block";
+	}
+}
