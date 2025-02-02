dfbb0667ee6c164f0617372be3bd597895061c9c
Jon Harvey <joncodes@proton.me>
2025-02-02 16:33:54 -0300

    Feature: Google analytics injection

--- jonharvey-tech/src/app/layout.tsx
+++ jonharvey-tech/src/app/layout.tsx
@@ -1,29 +1,58 @@
-import type { Metadata } from "next";
-import Link from "next/link";
+"use client";
 
-import Header from "./components/layouts/Header";
-import Footer from "./components/layouts/Footer";
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
-import { OverlayStoreProvider } from "./providers/OverlayStoreProvider";
-import { InputPositionStoreProvider } from "./providers/InputPositionStoreProvider";
-import { ShipStoreProvider } from "./providers/ShipStoreProvider";
+import Script from 'next/script';
 
-import "./globals.css";
-import "./styles/fonts.css";
+import Header from './components/layouts/Header';
+import Footer from './components/layouts/Footer';
 
-export const metadata: Metadata = {
-  title: "Jon Harvey | IT Business Solutions and Consulting",
-  description:
-    "Fast product ideation and prototyping for the growing enterprise -- Quality services, highly innovative and optimized solutions",
-};
+import { OverlayStoreProvider } from './providers/OverlayStoreProvider';
+import { InputPositionStoreProvider } from './providers/InputPositionStoreProvider';
+import { ShipStoreProvider } from './providers/ShipStoreProvider';
+
+import './globals.css';
+import './styles/fonts.css';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const pathname = usePathname(); // Get current route
+
+    useEffect(() => {
+      if (typeof window !== "undefined" && "gtag" in window) {
+        (window as any).gtag("config", "G-YNGB63JCR1", {
+            page_path: pathname,
+        });
+    }
+    }, [pathname]);
   return (
     <html>
+      <head>
+        {/* Google Analytics Script */}
+        <Script
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=G-YNGB63JCR1'
+        />
+        <Script
+          id='google-analytics'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-YNGB63JCR1', {
+                    page_path: window.location.pathname,
+                    });
+                  `,
+          }}
+        />
+      </head>
       <body>
         <Header />
         <main>
