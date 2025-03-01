837f67d031044dcf694c654c8ac53fa46216c47a
Jon Harvey <joncodes@proton.me>
2025-02-28 22:38:06 -0300

    Fix: contact button in Header links to /#contact now

--- jonharvey-tech/src/app/components/layouts/Header.tsx
+++ jonharvey-tech/src/app/components/layouts/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
         <a className="w-12 h-12 my-auto mx-2 rounded-full" href="/">
           <img className="rounded-full" src="/icons/favicon.png" />
         </a>
-        <a href="#contact" className="w-fit px-6 py-2 my-auto mx-2 h-12 text-center text-lg rounded-full border border-white transition duration-500 hover:bg-white hover:text-black hover:opacity-90 lg:text-xl font-bold">
+        <a href="/#contact" className="w-fit px-6 py-2 my-auto mx-2 h-12 text-center text-lg rounded-full border border-white transition duration-500 hover:bg-white hover:text-black hover:opacity-90 lg:text-xl font-bold">
           contact
         </a>
       </nav>
