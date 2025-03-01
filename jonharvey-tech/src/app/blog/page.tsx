51e36fe17b2d6a67d3c14aa0f476c0722bac0adb
Jon Harvey <joncodes@proton.me>
2025-02-28 22:53:46 -0300

    Fix: fixed layout shift for skeleton loader in blogcard

--- jonharvey-tech/src/app/blog/page.tsx
+++ jonharvey-tech/src/app/blog/page.tsx
@@ -28,25 +28,26 @@ export default function BlogLandingPage() {
   if (!posts) {
     return (
       <div className='w-full h-screen text-white font-Outfit text-center text-3xl mt-12'>
-        <ul className='px-4 md:w-1/2 mx-auto pt-20'>
+        <div className='w-full h-fit text-white font-Outfit text-center text-3xl mt-12'>
+        Latest Posts
+        <ul className='px-4 md:w-2/3 mx-auto pt-20'>
           {[...Array(5)].map((_, index) => (
-            <li
+              <li
               key={index}
-              className='h-fit border border-white rounded-3xl my-4 animate-pulse'
+              className='border border-white rounded-3xl w-full my-4 animate-pulse'
             >
-              <div className='border border-white rounded-3xl flex flex-col min-h-20 w-full my-4'>
-                <div className='relative block w-full h-64 rounded-2xl overflow-hidden shadow-lg transition-transform transform hover:scale-105'>
-                  <div className='absolute inset-0 w-full h-full bg-gray-300'></div>
-                  <div className='absolute inset-0 bg-gradient-to-t from-black to-transparent'></div>
-                  <div className='absolute bottom-0 left-0 w-full p-4 text-white'>
-                    <div className='h-8 bg-gray-400 rounded w-3/4 mb-2'></div>
-                    <div className='h-4 bg-gray-400 rounded w-1/4'></div>
-                  </div>
+              <div className='relative block w-full h-64 rounded-2xl overflow-hidden shadow-lg'>
+                <div className='absolute inset-0 w-full h-full bg-gray-300'></div>
+                <div className='absolute inset-0 bg-gradient-to-t from-black to-transparent'></div>
+                <div className='absolute bottom-0 left-0 w-full p-4 text-white'>
+                  <div className='h-8 bg-gray-400 rounded w-3/4 mb-2'></div>
+                  <div className='h-4 bg-gray-400 rounded w-1/4'></div>
                 </div>
               </div>
             </li>
           ))}
         </ul>
+        </ div>
       </div>
     );
   }
